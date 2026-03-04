872fa82d219451c8073a5e67243092b806794728
IFraimG <kulsasha55@gmail.com>
2026-03-04 20:52:32 +0300

    update release. Change API, divide classes, requestAnimationFrame instead of setInterval

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,14 @@
 
-import ProgressAPI from "./ProgressApi.js"
+import ProgressAPI from "../lib/ProgressApi.js"
 
 const normalNumber = document.getElementById("normal")
 const animateCheckbox = document.getElementById("animate")
 const hideCheckbox = document.getElementById("hide")
 const elementProgress = document.getElementById("c-pr")
 
-const progressAPI = new ProgressAPI(elementProgress)
+const templateOptions = { value: 90, animated: animateCheckbox.checked, hide: hideCheckbox.checked }
+
+const progressAPI = new ProgressAPI(elementProgress, templateOptions)
 
 normalNumber.addEventListener("change", event => progressAPI.updateValue(event.target.value))
 animateCheckbox.addEventListener("change", event => progressAPI.updateAnimatedOn(event.target.checked))
