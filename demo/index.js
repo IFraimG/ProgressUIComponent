8f1f7d9541dcd563e0d12808d11818e72bf957dd
IFraimG <kulsasha55@gmail.com>
2026-03-05 16:30:50 +0300

    load images for readme

--- demo/index.js
+++ demo/index.js
@@ -6,10 +6,10 @@ const animateCheckbox = document.getElementById("animate")
 const hideCheckbox = document.getElementById("hide")
 const elementProgress = document.getElementById("c-pr")
 
-const templateOptions = { value: 90, animated: animateCheckbox.checked, hide: hideCheckbox.checked }
+const templateOptions = { value: 80, animated: animateCheckbox.checked, hide: hideCheckbox.checked }
 
 const progressAPI = new ProgressAPI(elementProgress, templateOptions)
 
-normalNumber.addEventListener("change", event => progressAPI.updateValue(event.target.value))
-animateCheckbox.addEventListener("change", event => progressAPI.updateAnimatedOn(event.target.checked))
-hideCheckbox.addEventListener("change", event => progressAPI.updateHideOn(event.target.checked))
+// normalNumber.addEventListener("change", event => progressAPI.updateValue(event.target.value))
+// animateCheckbox.addEventListener("change", event => progressAPI.updateAnimatedOn(event.target.checked))
+// hideCheckbox.addEventListener("change", event => progressAPI.updateHideOn(event.target.checked))
